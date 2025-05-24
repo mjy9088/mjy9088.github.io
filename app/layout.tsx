a81208258dc14d457023e6e25d3051784ec0bb79
Juyeong Maing <mjy9088@Naver.com>
2025-05-24 17:37:14 +0900

    fix theme flash

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ import "./globals.css";
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `((theme) => (theme === "dark" || (!theme || theme === "system") && window.matchMedia("(prefers-color-scheme: dark)").matches) && document.documentElement.classList.add("dark"))(localStorage.getItem("theme"));`,
+          }}
+        />
+      </head>
       <body>
         <ThemeProvider
           attribute="class"
